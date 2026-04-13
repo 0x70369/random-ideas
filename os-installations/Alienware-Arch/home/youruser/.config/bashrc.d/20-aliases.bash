b2c30bcce66d11c53726b0b250f3f7d60a7634c5
0x70369 <caio.c.rtnn@proton.me>
2026-04-13 13:44:23 -0300

    new alias

--- os-installations/Alienware-Arch/home/youruser/.config/bashrc.d/20-aliases.bash
+++ os-installations/Alienware-Arch/home/youruser/.config/bashrc.d/20-aliases.bash
@@ -4,10 +4,9 @@
 # General
 # -----------------------------------------------------
 # Text editors
-alias v='nvim'
-alias vi='nvim'
-alias vim='nvim'
-alias n="nano"
+alias v="nvim"
+alias vi="nvim"
+alias vim="nvim"
 
 # -----------------------------------------------------
 # Shell
@@ -40,22 +39,20 @@ alias launch-plasma="/lib/plasma-dbus-run-session-if-needed /bin/startplasma-way
 # Aliases for configuring MATLAB launch commands - remember to check if the symbolic link exists at /usr/local/bin; if not, create it before running these (or edit the aliases themselves)
 
 # The aliases follow the instructions in these sections of the MATLAB article of the ArchWiki:
-# - https://wiki.archlinux.org/title/MATLAB#OpenGL_acceleration ---> Remember to create the java.opts file, if needed;
-# - https://wiki.archlinux.org/title/MATLAB#Running_on_Wayland
-# - https://wiki.archlinux.org/title/MATLAB#Desktop_entry
+## - https://wiki.archlinux.org/title/MATLAB#OpenGL_acceleration ---> Remember to create the java.opts file, if needed;
+## - https://wiki.archlinux.org/title/MATLAB#Running_on_Wayland
+## - https://wiki.archlinux.org/title/MATLAB#Desktop_entry
 
 ##alias matlab="env LD_PRELOAD=/usr/lib/libstdc++.so LD_LIBRARY_PATH=/usr/lib/xorg/modules/dri/ QT_QPA_PLATFORM=xcb /usr/local/bin/matlab -useStartupFolderPref"
 
 # The alias below has extra steps, documented in the following links:
-# - https://wiki.archlinux.org/title/PRIME#Configure_applications_to_render_using_GPU
-# - https://download.nvidia.com/XFree86/Linux-x86_64/570.153.02/README/primerenderoffload.html <--- Chapter 35 (accessed June 3rd, 2025; go to https://download.nvidia.com/XFree86/Linux-x86_64/ and navigate the tree to the latest version of the docs, or the one matching your driver's version)
+## - https://wiki.archlinux.org/title/PRIME#Configure_applications_to_render_using_GPU
+## - https://download.nvidia.com/XFree86/Linux-x86_64/570.153.02/README/primerenderoffload.html <--- Chapter 35 (accessed June 3rd, 2025; go to https://download.nvidia.com/XFree86/Linux-x86_64/ and navigate the tree to the latest version of the docs, or the one matching your driver's version)
 
 # Only enable this alias if the dGPU is active, so MATLAB can benefit from extra performance
 ##if [[ "$LIBVA_DRIVER_NAME" = nvidia ]] && [[ "$VDPAU_DRIVER" = nvidia ]]; then
-
-##alias MATLAB="env LD_PRELOAD=/usr/lib/libstdc++.so LD_LIBRARY_PATH=/usr/lib/xorg/modules/dri/ QT_QPA_PLATFORM=xcb prime-run /usr/local/bin/matlab -useStartupFolderPref -nosoftwareopengl"
-##alias MATLAB="env LD_PRELOAD=/usr/lib/libstdc++.so LD_LIBRARY_PATH=/usr/lib/xorg/modules/dri/ QT_QPA_PLATFORM=xcb __NV_PRIME_RENDER_OFFLOAD=1 __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0 __GLX_VENDOR_LIBRARY_NAME=nvidia __VK_LAYER_NV_optimus=NVIDIA_only /usr/local/bin/matlab -useStartupFolderPref -nosoftwareopengl"
-
+##    alias MATLAB="env LD_PRELOAD=/lib/libstdc++.so LD_LIBRARY_PATH=/lib/xorg/modules/dri/ QT_QPA_PLATFORM=xcb prime-run /usr/local/bin/matlab -useStartupFolderPref -nosoftwareopengl"
+##    alias MATLAB="env LD_PRELOAD=/lib/libstdc++.so LD_LIBRARY_PATH=/lib/xorg/modules/dri/ QT_QPA_PLATFORM=xcb __NV_PRIME_RENDER_OFFLOAD=1 __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0 __GLX_VENDOR_LIBRARY_NAME=nvidia __VK_LAYER_NV_optimus=NVIDIA_only /usr/local/bin/matlab -useStartupFolderPref -nosoftwareopengl"
 ##fi
 
 # Remember to install the packages mentioned in the sections linked above!
@@ -71,4 +68,4 @@ alias reformat-usb32G="sudo mkfs.exfat -b 4M -c 32K -v"
 
 # Repairing my USB flash drives (uses exfatprogs, install it if not already present) (inform the partition after the alias, e.g. /dev/sda1)
 # More details in: man fsck.exfat
-alias repair-exfat="sudo fsck.exfat -svp"
+alias repair-exfat="sudo fsck.exfat -spv"
